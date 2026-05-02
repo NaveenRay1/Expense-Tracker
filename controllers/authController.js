61b6b307e46e3d979af8c89a3af0c4755066fadb
Naveen Kumar Ray <218852547+NaveenRay1@users.noreply.github.com>
2026-05-02 15:59:54 +0530

    added auth

---
+++ controllers/authController.js
@@ -0,0 +1,58 @@
+const User = require('../models/User');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const sequelize = require('sequelize');
+
+const registerUser = async(req,res)=>{
+    try{
+        const {UserName,email,password} = req.body;
+        // now we can check them in database specially email if email exist then can't create
+        const user = await User.findOne({where:{email:email}});
+        if(user){
+            console.log('user already exists');
+            return res.status(400).json({message:'user already exist'});
+
+        }
+        // else we will create a user
+        // firstly hash the pass
+        const hashPass =await bcrypt.hash(password,10);
+       const data= await User.create({UserName,email,password:hashPass});
+        console.log('user created');
+        return res.status(201).json({message:'user created',data:data});
+    }
+    catch(err){
+        console.log(err);
+        return res.status(500).json({err:err});
+    }
+}
+
+const loginUser = async(req,res)=>{
+    try{
+        // gonna get data first
+        const {email,password} = req.body;
+        if(!email || !password)return res.status(400).json({msg:'email and pass cant be empty'})
+            
+        const user = await User.findOne({where:{email:email}});
+
+        if(!user)return res.status(404).json({msg:'email or password is incorrect'});
+        // if found compare password
+        const check = await bcrypt.compare(password,user.passowrd);
+        if(!check)return res.status(404).json({msg:'email or password is incorrect'});
+
+        const token = jwt.sign(
+            { id: user.id, email: user.email },  // payload
+            process.env.JWT_SECRET,               // secret key from .env
+            { expiresIn: '1h' }                   // token expires in 1 hour
+        );
+
+    return res.status(200).json({ message: 'successfully logged in', token: token });
+        console.log('sucess');
+        
+    }
+    catch(err) {
+        console.log(err);
+        return res.status(500).json({ err: err });
+    }
+}
+
+module.exports = {registerUser,loginUser};
--- server.js
+++ server.js
@@ -1,14 +1,24 @@
 const express = require('express');
 require('dotenv').config();
+const User = require('./models/User')
+const sequelize = require('./config/db')
+const authRoutes = require('./routes/authRoutes');
 const app = express();
 app.use(express.json());
 
-app.get('/',(req,res)=>{
-    console.log('api working');
-    return res.send('api working');
-})
 
+app.use('/auth',authRoutes);
 
-app.listen(3000,()=>{
-    console.log('server is running');
-})
+const startSever = async() =>{
+    try{
+        await sequelize.sync({alter:true})
+        .then(()=>console.log('databse connected'))
+        app.listen(process.env.PORT,()=>{
+            console.log('server is running at port',process.env.PORT);
+        })
+    }
+    catch(err){
+        console.log(err);
+    }
+}
+startSever();
